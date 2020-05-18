5ce52e9c15c444d4d54859d2eede10576530e8e9
ShadySentry <eurohawk@gmail.com>
2020-05-18 22:14:15 +0300

    ClinicServiceImplTest added

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceImplTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceImplTest.java
@@ -1,12 +1,58 @@
 package org.springframework.samples.petclinic.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.repository.PetRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
 class ClinicServiceImplTest {
+
+    private List<PetType> petTypes;
+
+    private PetType petUnderTest1;
+    private PetType petUnderTest2;
+
+    @InjectMocks
+    ClinicServiceImpl clinicService;
+
+    @Mock
+    PetRepository petRepository;
+
+    @BeforeEach
+    void initialize(){
+        petUnderTest1 = new PetType();
+        petUnderTest1.setName("Dog");
+        petUnderTest1.setId(1);
+
+        petUnderTest2 = new PetType();
+        petUnderTest2.setName("Cat");
+        petUnderTest2.setId(2);
+
+        petTypes = new ArrayList<>();
+        petTypes.add(petUnderTest1);
+        petTypes.add(petUnderTest2);
+    }
 
     @Test
     void findPetTypes() {
+        when(petRepository.findPetTypes()).thenReturn(petTypes);
+
+        List<PetType> returnedPets;
+        returnedPets = new ArrayList<>(clinicService.findPetTypes());
+        assertThat(returnedPets).isNotNull();
+        assertThat(returnedPets).size().isEqualTo(2);
+        assertThat(returnedPets).contains(petUnderTest1, petUnderTest2);
+
+
     }
 }
--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.web;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,53 +19,59 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class VetControllerTest {
 
-    private Map<String, Object> model;
-
-    private Vet vetUnderTest;
-
-    private final List<Vet> collectionOfVets = new ArrayList<>();
-    @Mock
+    @Mock(lenient = true)
     ClinicService clinicService;
 
     @InjectMocks
     VetController vetController;
+    private Map<String, Object> model;
+
+    private Vet vetUnderTest;
+
+    private List<Vet> collectionOfVets = new ArrayList<>();
 
 
-//    @Mock
+    //    @Mock
     private Vets vets;
 
-    @BeforeAll
-    void setUp(){
+    @BeforeEach
+    void setUp() {
         model = new HashMap<>();
 
         vetUnderTest = new Vet();
         vetUnderTest.setFirstName("John");
         vetUnderTest.setLastName("Boon");
         collectionOfVets.add(vetUnderTest);
+
+        when(clinicService.findVets()).thenReturn(collectionOfVets);
     }
 
     @Test
     void showVetList() {
-        when(clinicService.findVets()).thenReturn(collectionOfVets);
 
         String response = vetController.showVetList(model);
         assertThat(response).isEqualToIgnoringCase("vets/vetList");
 
         assertThat(model).hasSize(1);
 
-        List<Vet> retrievedVetsList=new ArrayList<>();
-        if(model.get("vets") instanceof Vets){
-            Vets retrievedVets = (Vets)model.get("vets");
+        List<Vet> retrievedVetsList = new ArrayList<>();
+        if (model.get("vets") instanceof Vets) {
+            Vets retrievedVets = (Vets) model.get("vets");
             retrievedVetsList = retrievedVets.getVetList();
         }
-        assertThat(retrievedVetsList).containsExactly(vetUnderTest);
+        assertThat(retrievedVetsList).contains(vetUnderTest);
     }
 
     @Test
     void showResourcesVetList() {
+        vets=null;
+        vets= vetController.showResourcesVetList();
+        List<Vet> vetsList=vets.getVetList();
+        assertThat(vetsList).contains(vetUnderTest);
     }
 }
