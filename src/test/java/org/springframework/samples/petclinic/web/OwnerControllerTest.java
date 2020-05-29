36f9787d40122f34097fce9b4572af7b23330763
ShadySentry <eurohawk@gmail.com>
2020-05-29 15:47:23 +0300

    testReturnOneOwnerObject creating

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,20 +1,19 @@
 package org.springframework.samples.petclinic.web;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml","classpath:spring/mvc-core-config.xml"})
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
     MockMvc mockMvc;
@@ -25,17 +24,42 @@ class OwnerControllerTest {
     @Autowired
     ClinicService clinicService;
 
-    @BeforeEach
-    void setUp(){
-        Object[] controllers;
-        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    Owner testOwner1;
+    Owner testOwner2;
 
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+        testOwner1 = new Owner();
+    }
+
+    @Test
+    void testFindByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                .param("lastName", "Dont find me"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testFindByNullLastName() throws Exception {
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testReturnOneOwnerObject() throws Exception {
+
+        mockMvc.perform(get("/owners")
+                .param("lastName","Dave"))
+                .andExpect(status().isOk());
     }
 
     @Test
     void initCreationForm() throws Exception {
-        mockMvc.perform(get("/owners/new")).
-                andExpect(status().isOk())
+        mockMvc.perform(get("/owners/new"))
+                .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
